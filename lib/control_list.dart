90dee1d7425d3f7087661120634ae6522264c21d
Bent Hillerkus <29630575+benthillerkus@users.noreply.github.com>
2023-06-22 02:00:19 +0200

    refactor: addendum to last commit

--- lib/control_list.dart
+++ lib/control_list.dart
@@ -1,22 +1,14 @@
 import 'package:flutter/widgets.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:smooth_shadow/configuration.dart';
 import 'package:smooth_shadow/control_groups/blur.dart';
 import 'package:smooth_shadow/control_groups/distance.dart';
 import 'package:smooth_shadow/control_groups/layers.dart';
 import 'package:smooth_shadow/control_groups/opacity.dart';
-import 'package:smooth_shadow/controls/controls.dart';
-import 'package:smooth_shadow/controls/curve.dart';
-import 'package:smooth_shadow/controls/slider.dart';
-import 'package:smooth_shadow/main.dart';
 
-class ConfiguratorList extends ConsumerWidget {
+class ConfiguratorList extends StatelessWidget {
   const ConfiguratorList({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final configuration = ref.watch(configurationProvider);
-
+  Widget build(BuildContext context) {
     return ListView(
       children: [
         for (final Widget widget in [
@@ -24,7 +16,6 @@ class ConfiguratorList extends ConsumerWidget {
           const OpacityControlGroup(),
           const DistanceControlGroup(),
           const BlurControlGroup(),
-
         ])
           Padding(
             padding: const EdgeInsets.only(bottom: 10.0),
